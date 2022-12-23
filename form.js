1afb88f4a5687cf88c76d215c4529fbdf31761a6
ZeyadShalaby <104474463+ZeyadShalaby@users.noreply.github.com>
2022-12-23 18:04:55 +0200

    Sign in form
    This does not include the pages linking or taking the data from the local storage yet or linking to the register page

---
+++ form.js
@@ -0,0 +1,18 @@
+//get the data from the local storage
+
+function signIn(e) {
+    let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    
+    //check if data exists in local storage
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+    if(!exist){
+        alert("Incorrect login credentials");
+    }
+    else{
+        //between the quotation mark insert whatever the home page is called
+        location.href = "";
+    }
+    e.preventDefault();
+}
